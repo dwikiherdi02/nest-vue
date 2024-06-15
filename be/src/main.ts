1dc7a50615bf08664d5583e524fa8a87ec52035a
DW-WSL <dwikiherdi520@gmail.com>
2024-06-15 14:36:50 +0700

    add CORS function

--- be/src/main.ts
+++ be/src/main.ts
@@ -4,6 +4,7 @@ import { ResponseInterceptor } from './response/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(process.env.APP_PORT);
 }
